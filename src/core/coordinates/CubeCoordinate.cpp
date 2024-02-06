742466e0a2356f9a22c3f8ada3dff4e4f9c6c47f
David MacGugan <dmagoo@users.noreply.github.com>
2024-02-06 01:25:00 -0500

    Fixed a typo in equality operator.

--- src/core/coordinates/CubeCoordinate.cpp
+++ src/core/coordinates/CubeCoordinate.cpp
@@ -2,9 +2,9 @@
 
 CubeCoordinate::CubeCoordinate(int q, int r, int s) : q_(q), r_(r), s_(s) {}
 
-bool CubeCoordinate::operator==(const Coordinate& other) const {
-    if (const CubeCoordinate* otherCube = dynamic_cast<const CubeCoordinate*>(&other)) {
-        return q_ == otherCube->q_ && r_ == otherCube->s_ && s_ == otherCube->s_;
+bool CubeCoordinate::operator==(const Coordinate &other) const {
+    if (const CubeCoordinate *otherCube = dynamic_cast<const CubeCoordinate *>(&other)) {
+        return q_ == otherCube->q_ && r_ == otherCube->r_ && s_ == otherCube->s_;
     }
     return false;
 }
